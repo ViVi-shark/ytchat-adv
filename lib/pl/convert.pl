ceec3d7f2c686be58b0f5c6b0e6b1bb91dea411b
ViVi <vivi.shark.tech+github@gmail.com>
2023-03-21 15:37:13 +0900

    『ブラドリウム』『ワールドエンドフロントライン』のユニット自動入力

--- lib/pl/convert.pl
+++ lib/pl/convert.pl
@@ -25,6 +25,8 @@ sub dataConvert {
   {
     my $sheetHtml = dataGet($set_url) or error 'URLを開けませんでした';
     my $bodyUrl;
+    my $gameName;
+    my $bodyFilter = undef;
     if ($sheetHtml =~ /\<link\s+rel=\"ytchat-body-exporting-point\"\s+type=\"[a-z0-9\/]+\"\s+href=\"(.+?)\"\s*\/?\>/) {
       $bodyUrl = $1;
       if (substr($bodyUrl, 0, 1) eq '.') {
@@ -33,6 +35,21 @@ sub dataConvert {
         $bodyUrl =~ s/\?.+$/$relation/;
         $bodyUrl =~ s#/\./#/#g;
       }
+    } elsif ($set_url =~ /^https:\/\/www\.fragment-sys\.com\/(bloodorium|wefl)\/make\.html\?id=(\d+)/) {
+      $gameName = $1;
+      my $characterId = $2;
+
+      my $systemCode;
+      if ($gameName eq 'bloodorium') {
+        $systemCode = 'bldrium';
+      } elsif ($gameName eq 'wefl') {
+        $systemCode = 'wldendfrnt';
+      } else {
+        error("Unsupported game: $gameName");
+      }
+
+      $bodyUrl = "https://20e41ubgh2.execute-api.ap-northeast-1.amazonaws.com/default/GetWebcharaItem/$systemCode/$characterId";
+      $bodyFilter = 'fragment-system';
     } else {
       error('ステータスの取得できる参照先ではありません');
     }
@@ -56,6 +73,13 @@ sub dataConvert {
     eval { $json = decode_json(join '', $data); };
     if ($@) { error('JSONの形式が不正です'); }
     my %pc = %{ $json };
+
+    if (defined($bodyFilter)) {
+      if ($bodyFilter eq 'fragment-system') {
+        %pc = %{convertFromFragmentSystem(\%pc, $gameName)};
+      }
+    }
+
     error('有効なキャラクターシートではありません') if !$pc{'ver'};
     my %stt;
     my @stt_name;
@@ -164,6 +188,178 @@ sub convertTags {
   $tags =~ tr/＋－＊／．，＿/\+\-\*\/\.,_/;
   $tags =~ tr/ / /s;
   return $tags
+}
+
+sub convertFromFragmentSystem {
+  my $referenceToSource = shift;
+  my $gameName = shift;
+
+  my %source = %$referenceToSource;
+  my %item = %{$source{'Item'}};
+
+  my $roleName = $item{'string1'} eq 'senteis' ? '剪定者' : $item{'string1'} eq 'adabana' ? ($gameName eq 'bloodorium' ? '徒花' : '不凋花') : undef;
+
+  my $className;
+  if ($gameName eq 'bloodorium') {
+    if ($item{'pcclass'} eq 'vamp' || $item{'pcclass'} eq 'class1') {
+      $className = 'ヴァンパイア';
+    }
+    elsif ($item{'pcclass'} eq 'knight' || $item{'pcclass'} eq 'class2') {
+      $className = '騎士';
+    }
+    elsif ($item{'pcclass'} eq 'rewrit' || $item{'pcclass'} eq 'class3') {
+      $className = 'リライター';
+    }
+  }
+  elsif ($gameName eq 'wefl') {
+    if ($item{'pcclass'} eq 'class1') {
+      $className = '亜神';
+    }
+    elsif ($item{'pcclass'} eq 'class2') {
+      $className = '妖精';
+    }
+    elsif ($item{'pcclass'} eq 'class3') {
+      $className = '契約者';
+    }
+  }
+
+  my %statuses = %{getFragmentSystemStatusesByRoleAndClass($roleName, $className)};
+  my %fragmentCounts = %{getFragmentSystemFragmentCounts(\%item, $roleName)};
+
+  my $cloudImageUrl = $item{'imageurl'};
+  my $hasImage = defined($cloudImageUrl) && !($cloudImageUrl eq '');
+  my $imageUrl;
+  if ($hasImage) {
+    if ($cloudImageUrl =~ /^https?:\/\/drive\.google\.com\/file\/d\/([A-Za-z0-9_\-.]+)(\/(view\?usp=(sharing|(?:share|drive)_link)(&|$)|edit))?/) {
+      $imageUrl = 'https://drive.google.com/uc?id=' . $1;
+    }
+    elsif ($cloudImageUrl =~ /^https:\/\/www\.dropbox\.com\/s\/.+[?&]dl=[01](&|$)/) {
+      $imageUrl = $cloudImageUrl;
+      $imageUrl =~ s/([?&]dl=)[01](&|$)/${1}1${2}/;
+    }
+  }
+  else {
+    $imageUrl = undef;
+  }
+
+  return {
+      'ver'               => 'x.x',
+      'characterName'     => $item{'pcname'},
+      'sheetDescriptionM' => makeFragmentSystemCharacterDescription($roleName, $className),
+      'unitStatus'        => [
+          { 'HP' => defined($statuses{'initialHp'}) ? "$statuses{'initialHp'}" : undef },
+          { 'HP初期値' => defined($statuses{'initialHp'}) ? "$statuses{'initialHp'}" : undef },
+          { 'ゲージ' => defined($statuses{'initialGauge'}) ? "$statuses{'initialGauge'}" : undef },
+          { 'ロスト数値' => defined($statuses{'lostThreshold'}) ? "$statuses{'lostThreshold'}" : undef },
+          { 'コレクトポイント' => '0' },
+          { 'フラグメント' => "$fragmentCounts{'available'}/$fragmentCounts{'all'}" },
+      ],
+      'image'             => $hasImage,
+      'imageURL'          => $imageUrl,
+      'imageFit'          => 'contain',
+      'imagePositionX'    => '50',
+      'imagePositionY'    => '50',
+  };
+}
+
+sub getFragmentSystemStatusesByRoleAndClass {
+  my $roleName = shift;
+  my $className = shift;
+
+  my $initialGauge = undef;
+  my $lostThreshold = undef;
+  my $initialHp = undef;
+
+  if ($roleName eq '剪定者') {
+    if ($className eq 'ヴァンパイア') {
+      $initialGauge = 5;
+      $lostThreshold = 0;
+      $initialHp = 13;
+    }
+    elsif ($className eq '騎士') {
+      $initialGauge = 6;
+      $lostThreshold = 0;
+      $initialHp = 15;
+    }
+    elsif ($className eq 'リライター') {
+      $initialGauge = 4;
+      $lostThreshold = 16;
+      $initialHp = 11;
+    }
+    elsif ($className eq '亜神') {
+      $initialGauge = 20;
+      $lostThreshold = 0;
+      $initialHp = 16;
+    }
+    elsif ($className eq '妖精') {
+      $initialGauge = 7;
+      $lostThreshold = 21;
+      $initialHp = 14;
+    }
+    elsif ($className eq '契約者') {
+      $initialGauge = 3;
+      $lostThreshold = 0;
+      $initialHp = 12;
+    }
+  }
+
+  return {
+      'initialGauge'  => $initialGauge,
+      'lostThreshold' => $lostThreshold,
+      'initialHp'     => $initialHp,
+  };
+}
+
+sub getFragmentSystemFragmentCounts {
+  my $referenceToCharacter = shift;
+  my $roleName = shift;
+
+  my %character = %$referenceToCharacter;
+
+  my $allCount = 0;
+  my $lostCount = 0;
+
+  my $maxFragmentCount = $roleName eq '剪定者' ? 6 : 20;
+
+  for (my $id = 73; $id <= (73 + $maxFragmentCount - 1); $id++) {
+    my $fragmentKey = 'string' . $id;
+    my $fragment = $character{$fragmentKey};
+
+    if (defined($fragment) && !($fragment eq '')) {
+      $allCount++;
+
+      my $lostKey = 'string' . ($id - 20);
+      my $lostState = $character{$lostKey};
+
+      if (defined($lostState) && !($lostState eq '' || $lostState == 0)) {
+        $lostCount++;
+      }
+    }
+  }
+
+  return {
+      'all'       => $allCount,
+      'available' => $allCount - $lostCount,
+  };
+}
+
+sub makeFragmentSystemCharacterDescription {
+  my $roleName = shift;
+  my $className = shift;
+
+  my @items = ();
+
+  if (defined($roleName) && !($roleName eq '')) {
+    push(@items, $roleName);
+  }
+
+  if (defined($className) && !($className eq '')) {
+    push(@items, "クラス: $className");
+  }
+
+  my $itemCount = @items;
+
+  return $itemCount > 0 ? join('／', @items) : undef;
 }
 
 1;
