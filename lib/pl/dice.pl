7ff9bafbafd2de8780d72ad38ed6688fa42dfcbd
ViVi <vivi.shark.tech+github@gmail.com>
2023-05-11 16:17:12 +0900

    『ブラドリウム』の【ダイスチェック】の結果に値を加算できるように

--- lib/pl/dice.pl
+++ lib/pl/dice.pl
@@ -56,9 +56,10 @@ sub diceCheck {
   }
   # BLOODORIUM
   elsif($::in{'game'} =~ /^bloodorium/){
-    if($comm =~ /^([0-9\+\-\*\/()]+DC|DC[0-9\+\-\*\/()]+)(?:\s|$)/i){
+    if($comm =~ /^(([0-9\+\-\*\/()]+DC|DC[0-9\+\-\*\/()]+)(\s*add\s*([0-9\+\-\*\/()]+))?)(?:\s|$)/i){
+      my $code = $1;
       require './lib/pl/dice/bloodorium.pl';
-      return bloodoriumDiceCheck($comm), 'dice:bloodorium';
+      return bloodoriumDiceCheck($comm), 'dice:bloodorium', $code;
     }
   }
 }
