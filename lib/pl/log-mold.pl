b7cd7520c307443cbdb28e64a5f7f345627935a8
ViVi <vivi.shark.tech+github@gmail.com>
2023-05-11 16:15:41 +0900

    『ブラドリウム』の「ブラッドガーデンマップ」を扱えるように

--- lib/pl/log-mold.pl
+++ lib/pl/log-mold.pl
@@ -63,6 +63,7 @@ $ROOM->param(roomId => $id);
 $ROOM->param(isPresent => !$::in{'log'} ? 1 : 0);
 $ROOM->param(title => $rooms{$id}{'name'});
 $ROOM->param(subtitle => $::in{'log'}?$::in{'log'}:'現行ログ');
+$ROOM->param(gameName => $game);
 
 $ROOM->param(liteMode => ($::in{'type'} eq 'lite') ? 1 : 0);
 $ROOM->param(dlMode => ($::in{'type'} eq 'download') ? 1 : 0);
@@ -181,6 +182,9 @@ foreach (<$FH>){
   }
   elsif($system =~ /^tab:([0-9]+)=(.*?)$/){
     if($2){ $tabs[$1-1] = "$2"; }
+  }
+  elsif($system =~ /^map/) {
+    $info = "<span class=\"map-update\" data-map-source=\"$info\" />";
   }
 
   if(!$tabs[$tab-1]){ $tabs[$tab-1] = "タブ${tab}"; }
