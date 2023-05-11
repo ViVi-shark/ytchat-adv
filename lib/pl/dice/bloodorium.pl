5f633533d4d71d2c382c009e8e2a3f11d4bfde47
ViVi <vivi.shark.tech+github@gmail.com>
2023-05-11 16:17:12 +0900

    『ブラドリウム』の【ダイスチェック】の結果に値を加算できるように

--- lib/pl/dice/bloodorium.pl
+++ lib/pl/dice/bloodorium.pl
@@ -20,6 +20,12 @@ sub bloodoriumDiceCheck {
         return '';
     }
 
+    my $additiveValueExpression = undef;
+
+    if ($comm =~ /^([0-9\+\-\*\/()]+DC|DC[0-9\+\-\*\/()]+)(\s*add\s*([0-9\+\-\*\/()]+))(?:\s|$)/i) {
+        $additiveValueExpression = $3;
+    }
+
     my $diceCount = int(calc($diceCountExpression));
 
     if ($diceCount < 1) {
@@ -27,6 +33,8 @@ sub bloodoriumDiceCheck {
     } elsif ($diceCount > 200) {
         return '';
     }
+
+    my $additiveValue = defined($additiveValueExpression) ? int(calc($additiveValueExpression)) : undef;
 
     my @diceValues = ();
     my %diceValueGroup = (1 => 0, 2 => 0, 3 => 0, 4 => 0, 5 => 0, 6 => 0);
@@ -68,11 +76,19 @@ sub bloodoriumDiceCheck {
 
     my $message = "【ダイスチェック： $diceCount 個】 ⇒ [$diceValuesText]";
 
+    my $finalValue = $maxDiceValue;
+
     if ($maxRepdigitCount > 1) {
-        my $finalValue = $maxDiceValue * $maxRepdigitCount;
+        $finalValue = $maxDiceValue * $maxRepdigitCount;
         $message .= " 最大値 $maxDiceValue ×《トライアンフ》 $maxRepdigitCount 倍！ = $finalValue";
     } else {
         $message .= " = $maxDiceValue";
+    }
+
+    if (defined($additiveValue) && $additiveValue != 0) {
+        $message .= ', ' . ($additiveValue > 0 ? '+' : '') . $additiveValue;
+        $finalValue += $additiveValue;
+        $message .= " ⇒ $finalValue";
     }
 
     return $message;
