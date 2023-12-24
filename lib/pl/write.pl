84817d0cea721a81fa75e5736a07f85a952ac086
ViVi <vivi.shark.tech+github@gmail.com>
2023-12-24 14:26:16 +0900

    Refactor: extract method to resolve asset's URL in GoogleDrive
    # Conflicts:
    #	lib/pl/write.pl

--- lib/pl/write.pl
+++ lib/pl/write.pl
@@ -113,10 +113,7 @@ else {
   # 挿絵 ----------
   elsif($::in{'comm'} =~ s<^/insert\s+(https?://.+)><>i){
     my $url = $1;
-    #Google
-    if($url =~ /^https?:\/\/drive\.google\.com\/file\/d\/(.+)\/view\?usp=(?:sharing|(?:share|drive)_link)$/){
-      $url = 'https://drive.google.com/uc?id=' . $1;
-    }
+    $url = resolveGoogleDriveAssetUrl($url); # Google
     $::in{'system'} = 'image';
     $::in{'comm'} = '';
     $::in{'info'} = $url;
@@ -156,10 +153,7 @@ else {
       }
       if(!$hit){ error('許可されていないURLです'); }
     }
-    #Google
-    if($url =~ /^https?:\/\/drive\.google\.com\/file\/d\/(.+)\/view\?usp=(?:sharing|(?:share|drive)_link)$/){
-      $url = 'https://drive.google.com/uc?id=' . $1;
-    }
+    $url = resolveGoogleDriveAssetUrl($url); # Google
     bgmEdit($url,$title,$volume);
     $::in{'name'} = "!SYSTEM";
     $::in{'comm'} = "BGMを変更 by $::in{'player'}";
@@ -192,10 +186,7 @@ else {
       }
       if(!$hit){ error('許可されていないURLです'); }
     }
-    #Google
-    if($url =~ /^https?:\/\/drive\.google\.com\/file\/d\/(.+)\/view\?usp=(?:sharing|(?:share|drive)_link)$/){
-      $url = 'https://drive.google.com/uc?id=' . $1;
-    }
+    $url = resolveGoogleDriveAssetUrl($url); # Google
     bgEdit($mode,$url,$title);
     $::in{'name'} = "!SYSTEM";
     $::in{'comm'} = "背景を変更 by $::in{'player'}";
@@ -972,6 +963,16 @@ sub pictureSettingsUpdate {
   print $FH decode('utf8', encode_json \%data);
   truncate($FH, tell($FH));
   close($FH);
+}
+
+sub resolveGoogleDriveAssetUrl {
+  my $url = shift;
+
+  if ($url =~ /^https?:\/\/drive\.google\.com\/file\/d\/(.+)\/view\?usp=(?:sharing|(?:share|drive)_link)$/) {
+    return 'https://drive.google.com/uc?id=' . $1;
+  }
+
+  return $url;
 }
 
 1;
