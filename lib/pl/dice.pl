49a615259cc121c2567f5b0a1532c32708e86fe3
ViVi <vivi.shark.tech+github@gmail.com>
2024-09-11 15:02:57 +0900

    ダイス一括ロールの最大回数を 50 に増加

--- lib/pl/dice.pl
+++ lib/pl/dice.pl
@@ -112,12 +112,12 @@ sub diceRoll {
   }
   elsif ($repeat =~ /^\d+$/) {
     @repeatLabels = ();
-    $repeat = ($repeat > 20) ? 20 : (!$repeat) ? 1 : $repeat;
+    $repeat = ($repeat > 50) ? 50 : (!$repeat) ? 1 : $repeat;
     push(@repeatLabels, '') foreach (1 .. $repeat);
   } else {
     @repeatLabels = split(/\s*,\s*/, $repeat);
     $repeat = @repeatLabels;
-    $repeat = 20 if $repeat > 20;
+    $repeat = 50 if $repeat > 50;
     @repeatLabels = @repeatLabels[0 .. ($repeat - 1)];
   }
 
