d4f28e25c38cf8ba77277d95f4c35c3421e62814
ViVi <vivi.shark.tech+github@gmail.com>
2024-10-10 07:23:51 +0900

    重複なしランダム選択コマンドの出力テキストにおいて、元の並び順を維持する
    # Conflicts:
    #	lib/pl/dice.pl

--- lib/pl/dice.pl
+++ lib/pl/dice.pl
@@ -344,9 +344,20 @@ sub shuffleRoll {
     $faces =~ s/</&lt;/;
     my @list = split(/[,、]/, $faces);
     return "" if (@list <= 1 || (!$rollsRaw)); #誤爆防止
-    @list = shuffle(@list);
-    my @choice = splice(@list, 0, $rolls);
-    return '<b>【✔:'.join(',', @choice).'】</b> <s>［×:'.join(',', @list).'］</s>', 'choice';
+    my @indexes = 0 .. $#list;
+    @indexes = shuffle(@indexes);
+    my @choiceIndexes = splice(@indexes, 0, $rolls);
+    @choiceIndexes = sort(@choiceIndexes);
+    @indexes = sort(@indexes);
+    my @choice = ();
+    foreach (@choiceIndexes) {
+      push(@choice, $list[$_]);
+    }
+    my @notChoice = ();
+    foreach (@indexes) {
+      push(@notChoice, $list[$_]);
+    }
+    return '<b>【✔:'.join(',', @choice).'】</b> <s>［×:'.join(',', @notChoice).'］</s>', 'choice';
   }
   return "";
 }
